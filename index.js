761d293fedb0bf3347b7357470e692013fb3db13
Dylan <email": null>
2016-08-09 13:38:42 +1200

    Updates

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ xhr.get(endpoint, function (err, data) {
 
   // In case you're curious
   console.log(data.body) // FYI: data.body is a string
-  var NewData = JSON.parse(data.body).id
+  var NewData = data.body
+  // var DataName = JSON.parse(data.body).name
   // Replace 'Space' below with the response
   var target = document.getElementsByTagName('main')[0]
   target.innerHTML = greeting({name: 'Dylan', NewData: NewData})
