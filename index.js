9d9ef913fc4fc0e76d522cc28a00b1bd7c96a28e
Dylan <email": null>
2016-08-09 17:20:56 +1200

    Updates

--- index.js
+++ index.js
@@ -3,26 +3,24 @@ var greeting = require('./views/greeting.hbs')
 
 var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
-function refreshButton() {
+xhr.get(endpoint, function (err, data) {
+  if (err) {
+    console.error(err)
+  }
 
-  xhr.get(endpoint, function (err, data) {
-    if (err) {
-      console.error(err)
-    }
+  // In case you're curious
+  console.log(data.body) // FYI: data.body is a string
+  // var newData = data.body
+  var issName = JSON.parse(data.body).name
+  var issLat = JSON.parse(data.body).latitude
+  var issLon = JSON.parse(data.body).longitude
 
-    // In case you're curious
-    console.log(data.body) // FYI: data.body is a string
-    // var newData = data.body
-    var issName = JSON.parse(data.body).name
-    var issLat = JSON.parse(data.body).latitude
-    var issLon = JSON.parse(data.body).longitude
+  // Replace 'Space' below with the response
 
-    // Replace 'Space' below with the response
+  var target = document.getElementsByTagName('main')[0]
+  target.innerHTML = greeting({name: 'Dylan', issName: issName, issLat: issLat, issLon: issLon})
+})
 
-    var target = document.getElementsByTagName('main')[0]
-    target.innerHTML = greeting({name: 'Dylan', issName: issName, issLat: issLat, issLon: issLon})
-  })
-}
 // var refreshButton = function() {
 //
 // }
