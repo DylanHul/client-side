e2fd43efa2a6eb766ae1387def3ced7f04d044ab
Dylan <email": null>
2016-08-09 18:36:48 +1200

    Updates

--- index.js
+++ index.js
@@ -22,17 +22,20 @@ xhr.get(endpoint, function (err, data) {
   var target = document.getElementsByTagName('main')[0]
   target.innerHTML = greeting({name: 'Dylan', issName: issName, issLat: issLat, issLon: issLon})
   document.getElementById('button').addEventListener('click', refreshButton)
+
+    function refreshButton() {
+          console.log(data.body)
+    }
 })
 
 
-function refreshButton() {
+
 //   xhr.get(endpoint, function (err, data) {
 //     if (err) {
 //       console.error(err)
 //     }
-    console.log(data.body)
+
 //     var dataRe = data.body
 //
 //     var target = document.getElementsByTagName('main')[0]
 //     target.innerHTML = button({dataRe: dataRe})
-}
