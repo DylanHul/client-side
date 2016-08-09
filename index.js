4faec734f96907bc0d80550caf976587652c23c2
Dylan <email": null>
2016-08-09 15:44:30 +1200

    Updates

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ xhr.get(endpoint, function (err, data) {
 
   // In case you're curious
   console.log(data.body) // FYI: data.body is a string
-  var newData = data.body
+  // var newData = data.body
   var issName = JSON.parse(data.body).name
   var issLat = JSON.parse(data.body).latitude
+  var issLon = JSON.parse(data.body).latitude
+
   // Replace 'Space' below with the response
+
   var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: 'Dylan', newData: issLat, issName: issName})
+  target.innerHTML = greeting({name: 'Dylan', issName: issName, issLat: issLat, issLon: issLon})
 })
