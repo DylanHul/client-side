099e75eb6139661a99c5d9e8432d86feab9da8ac
Dylan <email": null>
2016-08-09 18:12:02 +1200

    button working, but not function

--- index.js
+++ index.js
@@ -3,28 +3,35 @@ var greeting = require('./views/greeting.hbs')
 
 var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
-function refreshButton() {
+xhr.get(endpoint, function (err, data) {
+  if (err) {
+    console.error(err)
+  }
 
+  // In case you're curious
+  console.log(data.body) // FYI: data.body is a string
+  // var newData = data.body
+  var issName = JSON.parse(data.body).name
+  var issLat = JSON.parse(data.body).latitude
+  var issLon = JSON.parse(data.body).longitude
+
+
+  // Replace 'Space' below with the response
+
+  var target = document.getElementsByTagName('main')[0]
+  target.innerHTML = greeting({name: 'Dylan', issName: issName, issLat: issLat, issLon: issLon})
+  document.getElementById('button').addEventListener('click', refreshButton)
+})
+
+
+function refreshButton() {
   xhr.get(endpoint, function (err, data) {
     if (err) {
       console.error(err)
     }
 
-    // In case you're curious
-    console.log(data.body) // FYI: data.body is a string
-    // var newData = data.body
-    var issName = JSON.parse(data.body).name
-    var issLat = JSON.parse(data.body).latitude
-    var issLon = JSON.parse(data.body).longitude
-
-    // Replace 'Space' below with the response
+    var dataRe = JSON.parse(data.body).altitude
 
     var target = document.getElementsByTagName('main')[0]
-    target.innerHTML = greeting({name: 'Dylan', issName: issName, issLat: issLat, issLon: issLon})
-  })
+    target.innerHTML = buttonRender({dataRe: dataRe})
 }
-// var refreshButton = function() {
-//
-// }
-
-document.getElementsById('button').addEventListner(click, refreshButton)
