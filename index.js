f1a619d62f5b96904bc0e5fab6dd59ad2e650856
Dylan <email": null>
2016-08-10 12:02:37 +1200

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
 var button = require('./views/button.hbs')
+var newData = require('./views/newData.hbs')
 
 var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
