1e16decf29c0bafc1f97179d3227b056cd1a668f
Dylan <email": null>
2016-08-10 12:26:47 +1200

    ES6 added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const xhr = require('xhr')
-var greeting = require('./views/greeting.hbs')
-var button = require('./views/button.hbs')
+const greeting = require('./views/greeting.hbs')
+const button = require('./views/button.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
+const endpoint = 'https://api.wheretheiss.at/v1/satellites/25544'
 
 xhr.get(endpoint, function (err, data) {
   if (err) {
@@ -12,24 +12,24 @@ xhr.get(endpoint, function (err, data) {
   // In case you're curious
   console.log(data.body) // FYI: data.body is a string
   // var newData = data.body
-  var issName = JSON.parse(data.body).name
-  var issLat = JSON.parse(data.body).latitude
-  var issLon = JSON.parse(data.body).longitude
+  const issName = JSON.parse(data.body).name
+  const issLat = JSON.parse(data.body).latitude
+  const issLon = JSON.parse(data.body).longitude
 
 
   // Replace 'Space' below with the response
 
-  var target = document.getElementsByTagName('main')[0]
+  const target = document.getElementsByTagName('main')[0]
   target.innerHTML = greeting({name: 'Dylan', issName: issName, issLat: issLat, issLon: issLon})
 
   document.getElementById('button').addEventListener('click', refreshButton)
 
     function refreshButton() {
 
-        var issVel = JSON.parse(data.body).velocity
-        var issAlt = JSON.parse(data.body).altitude
+        const issVel = JSON.parse(data.body).velocity
+        const issAlt = JSON.parse(data.body).altitude
 
-        var target = document.getElementsByTagName('main')[0]
+        const target = document.getElementsByTagName('main')[0]
         target.innerHTML += button({issVel: issVel, issAlt:issAlt})
     }
 })
